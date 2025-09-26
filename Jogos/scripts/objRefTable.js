7dede922bd2259e3909ba99b90d85353ff35a565
Santosluiz12 <luiz.rpsantos12@gmail.com>
2025-09-26 13:35:43 -0300

    ad

---
+++ Jogos/scripts/objRefTable.js
@@ -0,0 +1,42 @@
+const C3 = self.C3;
+self.C3_GetObjectRefTable = function () {
+	return [
+		C3.Plugins.sliderbar,
+		C3.Plugins.Sprite,
+		C3.Behaviors.Rotate,
+		C3.Plugins.TextBox,
+		C3.Behaviors.solid,
+		C3.Behaviors.Physics,
+		C3.Behaviors.Platform,
+		C3.Behaviors.DragnDrop,
+		C3.Plugins.System.Cnds.EveryTick,
+		C3.Plugins.Sprite.Acts.SetSize,
+		C3.Plugins.sliderbar.Exps.Value,
+		C3.Behaviors.Rotate.Acts.SetSpeed,
+		C3.Plugins.System.Exps.int,
+		C3.Plugins.TextBox.Exps.Text
+	];
+};
+self.C3_JsPropNameTable = [
+	{BarraDeslizante: 0},
+	{Girar: 0},
+	{Sprite: 0},
+	{Sprite2: 0},
+	{EntradaDeTexto: 0},
+	{Sólido: 0},
+	{Física: 0},
+	{Plataforma: 0},
+	{Sprite3: 0},
+	{ArrastarSoltar: 0},
+	{Sprite4: 0}
+];
+
+self.InstanceType = {
+	BarraDeslizante: class extends self.ISliderBarInstance {},
+	Sprite: class extends self.ISpriteInstance {},
+	Sprite2: class extends self.ISpriteInstance {},
+	EntradaDeTexto: class extends self.ITextInputInstance {},
+	Plataforma: class extends self.ISpriteInstance {},
+	Sprite3: class extends self.ISpriteInstance {},
+	Sprite4: class extends self.ISpriteInstance {}
+}
